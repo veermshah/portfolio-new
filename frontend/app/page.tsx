1e68f20d6392fb572e72afb051532ab17bd5e863
Veer <veermickey@gmail.com>
2026-03-15 00:25:02 -0500

    nextjs setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Welcome to my portfolio!</h1>
+    </main>
+  );
+}
